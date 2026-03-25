965bcf391006351c985888be39e2f44dc239eadf
amiravalbaker <amiravb@yahoo.co.uk>
2026-03-25 17:53:31 +0000

    add regexes to settings

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,8 @@ CSRF_TRUSTED_ORIGINS = env_list("CSRF_TRUSTED_ORIGINS", [])
 CORS_ALLOW_ALL_ORIGINS = env_bool("CORS_ALLOW_ALL_ORIGINS", DEBUG)
 CORS_ALLOWED_ORIGINS = env_list("CORS_ALLOWED_ORIGINS", [])
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^http://localhost:\\d+$",
-    r"^http://127\\.0\\.0\\.1:\\d+$",
+    r"^http://localhost:\d+$",
+    r"^http://127\.0\.0\.1:\d+$",
 ]
 
 # Heroku fallback: avoid 400 Bad Request when env vars are not yet configured.
